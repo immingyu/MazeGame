9420bc76f7ee1523b44584076cb6d9dafba1badd
Leejoohyung-sejong <58721320+Leejoohyung-sejong@users.noreply.github.com>
2020-03-09 13:39:27 +0900

    Menu
    ui 테스팅 파일입니다.

---
+++ MazeGame_UI/Assets/Scripts/NameModify.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.UI;
+using UnityEngine;
+public class NameModify : MonoBehaviour
+{
+    // Start is called before the first frame update
+    public Text scripttext;
+
+    string tmp = "키키";
+    public void Debuging2()
+    {
+        scripttext.text = tmp.ToString();
+        Debug.Log("button click");
+    }
+}
